271e1ee3dd073f7f39c60bef0ba0f5ad296d13be
tri0n0 <116106958+tri0n0@users.noreply.github.com>
2022-10-18 23:58:29 +0530

    Create XOR.cpp
    This program takes two binary numbers as input and shows the output with the working of XOR gate.

---
+++ XOR.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+  
+{
+
+        int result,a,b;
+          cout<<" Enter the two numbers: ";
+            cin>>a>>b;
+                result = a^b;
+            cout<<" The output is: "<<result;
+          return 0;
+}
