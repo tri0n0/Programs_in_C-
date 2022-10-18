d9f43af58e309e861930a113d613f7863410f751
tri0n0 <116106958+tri0n0@users.noreply.github.com>
2022-10-18 23:23:26 +0530

    Create Input_String.cpp
    This program takes input from the user and shows it on the screen.

---
+++ Input_String.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+
+    char string[20];
+     cout<<" Enter the string: ";
+      cin.getline(string,20);
+     cout<<" The string is: "<<string;
+    return 0;
+}
