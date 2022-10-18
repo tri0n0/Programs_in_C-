f08627cb4db3958d26e5229cc592eab5288fbb7e
tri0n0 <116106958+tri0n0@users.noreply.github.com>
2022-10-18 23:27:21 +0530

    Create Sum.cpp
    This file takes two numbers from the user as input and adds it shows the summation.

---
+++ Sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+  
+{
+      int x,y,z;
+       cout<<" Enter the two numbers: ";
+          cin>>x>>y;
+              z = x+y;
+       cout<<" The summation is: "<<z;
+      return 0;
+}
