0a627e04cce94a3645aa780e74d7300eb4e5ff5c
tri0n0 <116106958+tri0n0@users.noreply.github.com>
2022-12-11 12:14:17 +0530

    Create Area.cpp
    In this program, we learn how to use the variables and the use of float data type and also calculate the area  of a circle using the radius given by the user.

---
+++ Area.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+  
+{
+  //here we are initialising the varibles.
+  int r;
+  float area;
+  //here we are asking for the radius.
+  cout<<" Enter the radius of the circle: ";
+  cin>>r;
+  //here we are calculating the radius.
+  
+  area = 3.14*r*r;
+  
+  //here we are displaying the area of the circle.
+  
+  cout<<" \n The area of the of the circle is: "<<area<<endl;
+  return 0;
+}
